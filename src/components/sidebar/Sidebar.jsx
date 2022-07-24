6758591d7d3bb480ef96c35a2506250cf1f1beb6
Thanh pham <thanhpham.23102000@gmail.com>
2022-07-24 16:34:21 +0700

    implement login page

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,27 +1,27 @@
-import "./sidebar.scss";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
-import LocalShippingIcon from "@mui/icons-material/LocalShipping";
-import CreditCardIcon from "@mui/icons-material/CreditCard";
-import StoreIcon from "@mui/icons-material/Store";
-import InsertChartIcon from "@mui/icons-material/InsertChart";
-import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
-import ExitToAppIcon from "@mui/icons-material/ExitToApp";
-import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
-import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
-import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
-import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
-import { Link } from "react-router-dom";
-import { DarkModeContext } from "../../context/darkModeContext";
-import { useContext } from "react";
+import './sidebar.scss';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import StoreIcon from '@mui/icons-material/Store';
+import InsertChartIcon from '@mui/icons-material/InsertChart';
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import SettingsSystemDaydreamOutlinedIcon from '@mui/icons-material/SettingsSystemDaydreamOutlined';
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import { Link } from 'react-router-dom';
+import { DarkModeContext } from '../../context/darkModeContext';
+import { useContext } from 'react';
 
 const Sidebar = () => {
   const { dispatch } = useContext(DarkModeContext);
   return (
     <div className="sidebar">
       <div className="top">
-        <Link to="/" style={{ textDecoration: "none" }}>
-          <span className="logo">lamadmin</span>
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <span className="logo">ADMIN</span>
         </Link>
       </div>
       <hr />
@@ -33,13 +33,13 @@ const Sidebar = () => {
             <span>Dashboard</span>
           </li>
           <p className="title">LISTS</p>
-          <Link to="/users" style={{ textDecoration: "none" }}>
+          <Link to="/users" style={{ textDecoration: 'none' }}>
             <li>
               <PersonOutlineIcon className="icon" />
               <span>Users</span>
             </li>
           </Link>
-          <Link to="/products" style={{ textDecoration: "none" }}>
+          <Link to="/products" style={{ textDecoration: 'none' }}>
             <li>
               <StoreIcon className="icon" />
               <span>Products</span>
@@ -89,11 +89,11 @@ const Sidebar = () => {
       <div className="bottom">
         <div
           className="colorOption"
-          onClick={() => dispatch({ type: "LIGHT" })}
+          onClick={() => dispatch({ type: 'LIGHT' })}
         ></div>
         <div
           className="colorOption"
-          onClick={() => dispatch({ type: "DARK" })}
+          onClick={() => dispatch({ type: 'DARK' })}
         ></div>
       </div>
     </div>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,164 @@
-import "./login.scss"
+import {
+  Box,
+  Grid,
+  Typography,
+  TextField,
+  Button,
+  CircularProgress,
+  colors,
+} from '@mui/material';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import './login.scss';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  console.log('ok');
+  // const { login } = useLogin();
+  // const [errMessage, setErrMessage] = useState('');
+  // const [loading, setLoading] = useState < boolean > false;
+  // const {
+  //   formState: { isValid },
+  //   control,
+  //   handleSubmit,
+  // } = useForm <
+  // User >
+  // {
+  //   resolver: yupResolver(loginSchema),
+  //   mode: 'onChange',
+  // };
+  // const dispatch = useAppDispatch();
+  // const loginselectorResult = useAppSelector(loginSelector);
+  // const navigate = useNavigate();
 
-export default Login
+  // useEffect(() => {
+  //   if (loginselectorResult.response?.data?.accessToken) {
+  //     return navigate('/user');
+  //   }
+  //   if (loginselectorResult.response?.message) {
+  //     setErrMessage(loginselectorResult.response.message);
+  //   }
+  // }, [navigate, loginselectorResult]);
+
+  // const formSubmitHandler = async (user) => {
+  //   setLoading(true);
+  //   try {
+  //     const { data } = await login(user);
+  //     dispatch(loginAsync(data));
+  //   } catch (err) {
+  //     return err;
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <Box
+          sx={{
+            minHeight: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <Box
+            component="form"
+            // onSubmit={handleSubmit(formSubmitHandler)}
+            sx={{
+              width: '376px',
+              mt: 6,
+            }}
+          >
+            <Typography
+              variant="h4"
+              sx={{
+                fontWeight: 'bold',
+                mb: 3,
+                '@media(min-height: 768px)': { mt: '150px' },
+                '@media(min-height: 920px)': { mt: '25vh' },
+              }}
+            >
+              Đăng nhập vào tài khoản
+            </Typography>
+            <Box>
+              <Box sx={{ mb: 2 }}>
+                <Typography component="label" variant="body1">
+                  Tài khoản đăng nhập hoặc email
+                </Typography>
+                <TextField
+                  fullWidth
+                  placeholder="123456789"
+                  sx={{ root: { height: '50px' }, mt: 1 }}
+                />
+              </Box>
+              <Box>
+                <Typography component="label" variant="body1">
+                  Mật khẩu
+                </Typography>
+                <TextField
+                  fullWidth
+                  type="password"
+                  placeholder="***********"
+                  sx={{ root: { height: '50px' }, mt: 1 }}
+                />
+              </Box>
+              <Box mt={2} mb={3}>
+                <Typography
+                  mb={2}
+                  align="right"
+                  sx={{ cursor: 'pointer', color: colors.indigo['600'] }}
+                >
+                  <Link to="/forgot-password">Quên mật khẩu?</Link>
+                </Typography>
+                <Button
+                  fullWidth
+                  sx={{
+                    background: colors.green['400'],
+                    color: '#fff',
+                    height: '50px',
+                    '&:hover': {
+                      background: colors.green['600'],
+                    },
+                  }}
+                  variant="contained"
+                  // disabled={Boolean(!isValid || loading)}
+                  type="submit"
+                  // startIcon={loading && <CircularProgress size={20} />}
+                >
+                  Đăng nhập
+                </Button>
+              </Box>
+              <Box>
+                <Typography my={3}>
+                  Hoặc đăng ký tài khoản, nếu bạn chưa đăng ký !
+                </Typography>
+                <Button
+                  fullWidth
+                  component={Link}
+                  to={'/register'}
+                  sx={{
+                    color: colors.green['400'],
+                    height: '50px',
+                    '&:hover': {
+                      background: colors.green['100'],
+                    },
+                    borderColor: colors.green['400'],
+                    border: 1,
+                  }}
+                  type="submit"
+                >
+                  Đăng ký
+                </Button>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Login;
